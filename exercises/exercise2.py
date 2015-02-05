6f25b8b2ab671eafdcc09840f319cd9179aa5101
Michael-Sadowsky <micsadow@umich.edu>
2015-02-05 16:57:03 -0500

    A few small changes

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -1,24 +1,29 @@
-L=[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+List=[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144] #an example list
 
-tot=0.0
-count=0.0
-for i in L:
-	tot=tot+i
-	count=count+1
-mean=tot/count
-print 'This is the mean:', mean
+def mean(List):
+	'''
+    Sum a list and divide by its length to get the mean
+    '''
+	tot=0.0 #A variable to hold the total
+	count=0.0 #A variable to increment and ultimately divide by
+	for i in List:
+		tot=tot+i #increment the total by an element in the list
+		count=count+1 #increment the count by 1
+	return tot/count #return the mean
+print 'This is the mean:', mean(List) #run the function, printing off the list
 
-x=55
-
-def binary_search(x,list,left,right):
-	if left>right:
+def binary_search(find_this,list,left,right):
+	'''
+    Go through a sorted list, and find the position of a specific value, recursively dividing the list in half
+    '''
+	if left>right: #if there's an error in the input, return -1
 		return -1
-	mid=(left+right)/2
-	if L[mid]==x:
+	mid=(left+right)/2 #Get the midpoint of the list
+	if list[mid]==find_this: #The base case; if the middle is found, end the recursion and return mid
 		return mid
-	elif L[mid]>x:
-		return binary_search(x,list,left,mid-1)
-	elif L[mid]<x:
-		return binary_search(x,list,mid+1,right)
+	elif list[mid]>find_this: #Continue searching in the left side of the list if mid is too high; use a recursive call
+		return binary_search(find_this,list,left,mid-1)
+	elif list[mid]<find_this:#Continue searching in the righ side of the list if mid is too low; use a recursive call
+		return binary_search(find_this,list,mid+1,right)
 
-print(binary_search(x,list,0,len(L)-1))
+print(binary_search(55,List,0,len(List)-1)) #run the binary_search function
